c0942007a75b103831a39c50cb3d7db56d077f81
Victor Campos <victor.campos@visagio.com>
2016-03-13 11:10:56 -0300

    fetch spreadsheet as csv and add to site.data

--- _plugins/jekyll_get.rb
+++ _plugins/jekyll_get.rb
@@ -1,40 +1,32 @@
-require 'json'
-require 'hash-joiner'
+require 'csv'
 require 'open-uri'
+require 'pry'
+require 'json'
 
 module Jekyll_Get
   class Generator < Jekyll::Generator
     safe true
     priority :highest
 
+    ONG_CSV_URL = "https://docs.google.com/spreadsheets/d/1NQAfOgeO8CwYVzDDjkATrHZ9RssyyVzvl_CYYBVIU9U/pub?output=csv"
+
     def generate(site)
-      config = site.config['jekyll_get']
-      if !config
-        return
+      ongs= []
+
+      source = CSV.new(open(ONG_CSV_URL).read, headers: :first_row).each do |row|
+        ongs << fetch_ong_from_csv_row(row) if row[5] == "Aprovado"
       end
-      if !config.kind_of?(Array)
-        config = [config]
-      end
-      config.each do |d|
-        begin
-          target = site.data[d['data']]
-          source = JSON.load(open(d['json']))
-          if target
-            HashJoiner.deep_merge target, source
-          else
-            site.data[d['data']] = source
-          end
-          if d['cache']
-            data_source = (site.config['data_source'] || '_data')
-            path = "#{data_source}/#{d['data']}.json"
-            open(path, 'wb') do |file|
-              file << JSON.generate(site.data[d['data']])
-            end
-          end
-        rescue
-          next
-        end
-      end
+
+      site.data["ongs"] = ongs.to_json
+    end
+
+    def fetch_ong_from_csv_row(row)
+      {
+        "name" => row[1].encode!("utf-8", "utf-8", :invalid => :replace),
+        "email" => row[2].encode!("utf-8", "utf-8", :invalid => :replace),
+        "description" => row[3].encode!("utf-8", "utf-8", :invalid => :replace),
+        "needs" => row[4].encode!("utf-8", "utf-8", :invalid => :replace)
+      }
     end
   end
 end
