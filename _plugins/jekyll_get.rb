cec4f42c59dcab0c58504e9468615446c38bc1e8
Victor Campos <victor.campos@visagio.com>
2016-03-16 07:05:21 -0300

    fetch dev data from form

--- _plugins/jekyll_get.rb
+++ _plugins/jekyll_get.rb
@@ -8,20 +8,52 @@ module Jekyll_Get
     priority :highest
 
     ONG_CSV_URL = "https://docs.google.com/spreadsheets/d/1NQAfOgeO8CwYVzDDjkATrHZ9RssyyVzvl_CYYBVIU9U/pub?output=csv"
+    DEV_CSV_URL = "https://docs.google.com/spreadsheets/d/1IT3Uo2YW5mv2iVjThRfj2Uc4Ac0yFXlWu4ZCnbKBVaU/pub?&output=csv"
+    GITHUB_API  = "https://api.github.com"
 
     def generate(site)
-      site.data["ongs"] = CSV.new(open(ONG_CSV_URL).read, headers: :first_row).select { |row| row[5] == "Aprovado" }.map do |row|
-        fetch_ong_from_csv_row(row)
+      site.data["ongs"] = fetch_ongs
+      site.data["devs"] = fetch_devs
+    end
+
+    def fetch_ongs
+      fetch_csv(ONG_CSV_URL) { |row| fetch_ong_from_csv_row(row) }
+    end
+
+    def fetch_devs
+      fetch_csv(DEV_CSV_URL) { |row| fetch_dev_from_csv_row(row) }
+    end
+
+    def fetch_csv(url)
+      CSV.new(open(url), headers: :first_row).select { |row| row[-1] == "Aprovado" }.map do |row|
+        yield(row)
       end
+    end
+
+    def fetch_dev_from_csv_row(row)
+      {
+        "name" =>  fetch_cell(row, 1),
+        "email" =>  fetch_cell(row, 2),
+        "description" =>  fetch_cell(row, 3),
+        "github" =>  fetch_github(fetch_cell(row, 4))
+      }
     end
 
     def fetch_ong_from_csv_row(row)
       {
-        "name" => row[1].encode!("utf-8", "utf-8", :invalid => :replace),
-        "email" => row[2].encode!("utf-8", "utf-8", :invalid => :replace),
-        "description" => row[3].encode!("utf-8", "utf-8", :invalid => :replace),
-        "needs" => row[4].encode!("utf-8", "utf-8", :invalid => :replace)
+        "name" => fetch_cell(row, 1),
+        "email" =>  fetch_cell(row, 2),
+        "description" =>  fetch_cell(row, 3),
+        "needs" =>  fetch_cell(row, 4)
       }
+    end
+
+    def fetch_cell(row, cell)
+      row[cell] && row[cell].encode!("utf-8", "utf-8", invalid: :replace)
+    end
+
+    def fetch_github(username)
+      username && JSON.load(open("#{GITHUB_API}/users/#{username}"))
     end
   end
 end
